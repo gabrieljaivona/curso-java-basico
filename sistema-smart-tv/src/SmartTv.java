7be8d1f45da57b644d315f7a443df54a2a72df53
GabrielJFS <166074513+gabrieljaivona@users.noreply.github.com>
2024-08-26 17:40:39 -0400

    Termino do exercicio SmartTV

--- sistema-smart-tv/src/SmartTv.java
+++ sistema-smart-tv/src/SmartTv.java
@@ -1,9 +1,25 @@
 public class SmartTv {
     boolean ligada = false;
-    int canal = 1;
     int volume = 25;
+    int canal = 1;
 
-    public void ligar(){
+    public void ligar(){ 
         ligada = true;
     }
+    public void desligar(){
+        ligada = false;
+    }
+    public void aumentarVolume(){
+        volume+=5;
+    }
+    public void diminuirVolume(){
+        volume-=5;
+    }
+    public void passarCanal(){
+        canal++;
+    }
+    public void voltarCanal(){
+        canal--;
+    }
+
 }
--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -4,8 +4,23 @@ public class Usuario {
         SmartTv smartTv = new SmartTv();
 
         System.out.println("TV Ligada ?" + smartTv.ligada);
-        System.out.println("Canal Atual: " +smartTv.canal);
+        System.out.println("Canal Atual: " + smartTv.canal);
         System.out.println("Volume Atual : " +smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Novo status -> TV ligada ? " + smartTv.ligada);
+
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        smartTv.passarCanal();
+        System.out.println("Canal atual: " + smartTv.canal);
 
     }
 }
